85efb3434ed9561ad8999a412d504c935e117943
pitlanefantasy <pitlanefantasy@gmail.com>
2026-05-26 11:01:06 +0000

    add terraform base configuration

---
+++ infrastructure/terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "project_id" {
+    description = "Proyecto GCP"
+    type        = string
+    default     = "pitlane-fantasy"    
+}
+
+variable "region" {
+    description = "Region GCP - región (Bélgica)"
+    type        = string
+    default     = "europe-west1"
+}
+
+variable "zone" {
+    description = "Zona principal de GCP"
+    type        = string
+    default     = "europe-west1-b"
+}
+
