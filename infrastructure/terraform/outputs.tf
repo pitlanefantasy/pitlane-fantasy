3be974dd94559ff8aec2b4fa66eb44de8c349c2e
pitlanefantasy <pitlanefantasy@gmail.com>
2026-05-26 12:25:32 +0000

    add outputs.tf

---
+++ infrastructure/terraform/outputs.tf
@@ -0,0 +1,10 @@
+output "project_id" {
+    description = " Porjecto fantasy"
+    value       = var.project_id
+}
+
+output "region" {
+    description = " Region Belgica"
+    value       = var.region
+}
+
