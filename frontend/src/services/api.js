337e6fc61c1bd0174405729039ed9ffc75d528b3
pitlanefantasy <pitlanefantasy@gmail.com>
2026-07-14 09:48:11 +0000

    deploy frontend to Kubernetes - production live

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.153:8000',
+  baseURL: 'http://34.62.203.185',
   headers: {
     'Content-Type': 'application/json',
   },
